ea7c10ec74eab05744014e279d48846042fb8f97
Gabriel <gabrielnfa999@gmail.com>
2022-04-16 12:30:51 -0300

    register page created

---
+++ lib/Screen/register.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro"),
+        backgroundColor: const Color.fromARGB(255, 239, 121, 91),
+        elevation: 0,
+      ),
+      body: Container(
+        decoration:
+            const BoxDecoration(color: Color.fromARGB(255, 239, 121, 91)),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "images/logo/logologin.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  //INPUT EMAIL
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(22),
+                        )),
+                  ),
+                ),
+                TextField(
+                  // INPUT SENHA
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22),
+                      )),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                          padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(22)),
+                          backgroundColor:
+                              const Color.fromARGB(255, 101, 187, 88)),
+                      child: const Text(
+                        "Cadastrar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
