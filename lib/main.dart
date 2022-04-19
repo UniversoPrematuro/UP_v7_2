5354718bd977df9e05fcf9df117618ab38e702ca
Gabriel <gabrielnfa999@gmail.com>
2022-04-19 09:55:50 -0300

    unseted Firebase data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'Screen/login.dart';
+import 'package:universoprem_v7_2/Screen/home.dart';
+import 'Screen/home.dart';
+import 'Screen/register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseFirestore.instance
-      .collection("users")
-      .doc("001")
-      .set({"nome": "Gabriel"});
+  FirebaseFirestore.instance.collection("users").doc().set({});
 
   runApp(MaterialApp(
-      home: const Login(),
+      home: const Home(),
       theme: ThemeData(
           primaryColor: const Color.fromARGB(255, 239, 121, 91),
           colorScheme: ColorScheme.fromSwatch().copyWith(
@@ -23,6 +23,8 @@ void main() async {
               style: TextButton.styleFrom(
             elevation: 20,
             shadowColor: Colors.black,
-          )) // Color Scheme
+          ) // Style From
+              ) // Color Scheme
+
           )));
 }
