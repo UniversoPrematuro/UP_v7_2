441742a3068dc2d7b2d910afcf763db93147284b
Gabriel <gabrielnfa999@gmail.com>
2022-04-20 10:25:53 -0300

    profile added

---
+++ lib/Screen/profile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'register.dart';
+import 'login.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro"),
+        backgroundColor: const Color.fromARGB(255, 239, 121, 91),
+        elevation: 0,
+      ),
+      body: Container(
+        decoration:
+            const BoxDecoration(color: Color.fromARGB(255, 239, 121, 91)),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "images/logo/logologin.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  //INPUT NOME
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Nome",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(22),
+                        )),
+                  ),
+                ),
+                Padding(
+                  //INPUT EMAIL
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(22),
+                        )),
+                  ),
+                ),
+                TextField(
+                  // INPUT SENHA
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22),
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
