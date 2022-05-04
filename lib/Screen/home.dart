4029237ee896e7b387d04c3e9cc9de4b5eb8408a
Gabriel <gabrielnfa999@gmail.com>
2022-05-04 10:54:04 -0300

    home done

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:universoprem_v7_2/Screen/profile.dart';
 import 'package:universoprem_v7_2/Screen/register.dart';
-
-import 'login.dart';
+import 'package:universoprem_v7_2/Screen/login.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -20,53 +18,71 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: const Color.fromARGB(255, 101, 188, 88),
-        label: const Text(
-          'Entrar',
-          style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-          ),
-        ),
-        icon: const Icon(Icons.forward,
-            color: Color.fromARGB(255, 255, 255, 255)),
-        onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => const Login()));
-        },
-      ),
+      // appBar: AppBar(
+      //   toolbarHeight: 14,
+      //   backgroundColor: const Color.fromARGB(255, 255, 193, 143),
+      //   elevation: 0,
+      // ),
       body: Padding(
           padding:
-              const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 15),
-          child: Stack(
-            children: <Widget>[
-              Center(
-                child: Image.asset(
-                  "images/logo/LogoPrinc.png",
-                  width: 230,
+              const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 5),
+          child: Stack(children: <Widget>[
+            Center(
+              child: Image.asset(
+                "images/logo/LogoMov.gif",
+                width: 280,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 500, left: 13),
+              child: ElevatedButton(
+                onPressed: (() {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Register()));
+                }),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 255, 255, 255)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14))),
+                    minimumSize: MaterialStateProperty.all(const Size(75, 40)),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            vertical: 12.5, horizontal: 40.0))),
+                child: GestureDetector(
+                  child: const Text(
+                    "Primeira vez no Universo Prematuro?",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 101, 187, 88),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
+                  ),
                 ),
               ),
-              Padding(
-                  padding: const EdgeInsets.only(top: 55),
-                  child: Center(
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Register()));
-                      },
-                      child: const Text(
-                        "Primeiro Cadastro",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w900,
-                            fontSize: 22),
-                      ),
-                    ),
-                  ))
-            ],
-          )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 445, left: 45),
+              child: ElevatedButton(
+                onPressed: (() {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Login()));
+                }),
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14))),
+                    minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 40.0))),
+                child: const Text(
+                  "Já tem uma conta? Entre!",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
+            )
+          ])),
     );
   }
 }
